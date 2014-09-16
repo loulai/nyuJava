28d418fa59854a2d9f5431b9a4fae96a83f03b74
loulai <louiselai88@gmail.com>
2014-09-16 14:02:56 -0400

    added load of homework

--- Dice/src/Main.java
+++ Dice/src/Main.java
@@ -1,7 +1,20 @@
 
+/*Author     : Louise (Lou) Lai
+ *Created on : September 13th 2014
+ *Description: Program stimulates rolling a dice 100,00 times. It prints the occurrence,
+ *             in percentage, of each result.
+ */
+
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		// Concept: six 'counters' are responsible for keeping count of the number
+		// of times a dice face is rolled. i.e. 'oneCounter' keeps track of the 
+		// number of times 1 is rolled.
+		
+		// First I initialize each counter as an integer.
 		
 		int oneCounter = 0;
 		int twoCounter = 0;
@@ -10,33 +23,42 @@ public class Main {
 		int fiveCounter = 0;
 		int sixCounter = 0;
 		
-		for (int i = 0; i < 10000; i++) {
+		// The for loop runs 100,000 times
+		
+		for (int i = 0; i < 100000; i++) {
 			
-			Double myDouble = Math.random() * 6;
+			// Within the for loop..
 			
-			int myInt = myDouble.intValue();
+			//.. a random number is generated, and modified to span
+			//   numbers 0.0 to 6.0 (non-inclusive).
+			Double dice = Math.random() * 6.0;
 			
-			if (myInt == 0) {
+			//.. each counter is incremented by one when their 
+			//   respective dice face is rolled.
+			if (dice < 1) {
 				oneCounter++;
-			} else if (myInt == 1) {
+			} else if (dice < 2) {
 				twoCounter++;
-			} else if (myInt == 2) {
+			} else if (dice < 3) {
 				threeCounter++;
-			} else if (myInt == 3) {
+			} else if (dice < 4) {
 				fourCounter++;
-			} else if (myInt == 4) {
+			} else if (dice < 5) {
 				fiveCounter++;
 			} else {
 				sixCounter++;
 			}
 		}
 		
-		System.out.println("One Counter has: " + oneCounter);	
-		System.out.println("Two Counter has: " + twoCounter);
-		System.out.println("Three Counter has: " + threeCounter);
-		System.out.println("One Counter has: " + fourCounter);	
-		System.out.println("Two Counter has: " + fiveCounter);
-		System.out.println("Three Counter has: " + sixCounter);
+
+		// prints the percentage of times dice face is rolled.
+		System.out.printf("1 was rolled %f%% of the times. \n", + (oneCounter/100000.0)*100.0);	
+		System.out.printf("2 was rolled %f%% of the times. \n", + (twoCounter/100000.0)*100.0);
+		System.out.printf("3 was rolled %f%% of the times. \n", + (threeCounter/100000.0)*100.0);
+		System.out.printf("4 was rolled %f%% of the times. \n", + (fourCounter/100000.0)*100.0);	
+		System.out.printf("5 was rolled %f%% of the times. \n", + (fiveCounter/100000.0)*100.0);
+		System.out.printf("6 was rolled %f%% of the times. \n", + (sixCounter/100000.0)*100.0);
+	
 		
 	}
 
---
+++ Letters/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		switch(input) {
+		
+		case 1:
+			System.out.print
+		}
+
+	}
+
+}
