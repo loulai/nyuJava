e2ea0eae817a66201bf211c7e13f5055088d60ea
loulai <louiselai88@gmail.com>
2014-10-07 17:13:37 -0400

    finished question 2

--- FindingLargest/src/FindingLargest.java
+++ FindingLargest/src/FindingLargest.java
@@ -7,17 +7,22 @@ public class FindingLargest {
 		System.out.print("Type number: ");
 		Scanner input = new Scanner(System.in);
 		int max = input.nextInt();
-		int num = 2;
-	
+		int num = 1;
+		int counter = 1;
+
 		while (num != 0) {
 			num = input.nextInt();
 			if (num > max && num != 0){
 				max = num;
+				counter = 1;
+				System.out.println(max);
+			} else if (num == max) {
+				counter ++;
 			}
 		}
 		
-		System.out.println(max);
-		
+		System.out.println(max + " occurs " + counter + " times." );
+	
 	}
 
 }
