dd9d99927ae9f9f7587bc1c3ab574252f3eca85b
loulai <louiselai88@gmail.com>
2014-10-06 22:48:18 -0400

    new projects; sorting through strings for largest integer

--- ClassWork/src/Main.java
+++ ClassWork/src/Main.java
@@ -1,35 +1,33 @@
+import java.util.Scanner;
+
 
 public class Main {
-
+	
 	public static void main(String[] args) {
 		
-		int[] list1 = new int[10];
-		int[] list2 = new int[10];
-		int[] list3 = new int[10];
-		
-		for(int i = 0; i < list1.length; i++) {
-			list1[i] = (int) (Math.random() * 1501);
-			list2[i] = (int) (Math.random() * 1501);
-			
-			list3[i] = list1[i] - list2[i];
-		}
-		
-		for(int i = 0; i < 3; i++) {
-			System.out.println((i + 1)+ ")" + list1[i]);
-			System.out.println("- " + list2[i]);
-			System.out.println("= " + list3[i]);
-		}
-	
-		
-		
-		
-		
-		
-		
-		
-		
-			
-		}
+		int number = 0;
 
+		int[] numArr = new int[1];
+		numArr[0]=0;
+		
+		m(number, numArr);
+		
+		System.out.println("number: " + number);
+		System.out.println("number array: " + numArr[0]);
+		
+		int count = 0;
+		do{
+			System.out.println(count + " hello");
+		} while (count++ < 9);
+		
+			System.out.println(count);
+			
 	}
+	
+	public static void m(int someNumber, int[] someArray) {
+		someNumber = 3;
+		someArray[0] = 3;
+	}
+	
+}
 
