74a087f60dc180002c97a1adf90a60125b8d5ded
loulai <louiselai88@gmail.com>
2014-10-22 23:55:19 -0400

    can generate and print matrices perfectly

--- Matrix/src/Main.java
+++ Matrix/src/Main.java
@@ -18,16 +18,23 @@ public class Main {
 	
 	public static int[][] generateMatrix(int row, int column) {
 		int[][] matrix = new int[row][column];
+		
+		for(int i = 0; i < row; i++) {
+			for(int j = 0; j< column; j++) {
+				matrix[i][j] = (int) (Math.random() * 21) - 10; // first generates numbers 0 to 20, then subtracts by 10 to get range of -10 to 10
+				}
+			}
 		return matrix;
 	}
 	
 	public static void printMatrix(int[][] matrix){
-		int rowSize = matrix.length; //counts the number of rows
-		int columnSize = matrix[0].length; // counts number of columns
-		for(int i = 0; i < 2; i++) {
-			for(int j = 0; i< 2; j++) {
-				System.out.print(matrix[i][j]);
+		int rowSize = matrix.length;       //counts the number of rows
+		int columnSize = matrix[0].length; //counts number of columns
+		for(int i = 0; i < rowSize; i++) { //outer loop runs 'rowSize' times
+			for(int j = 0; j< columnSize; j++) { //inner loop runs 'columnSize' times
+				System.out.printf("%3d", matrix[i][j]); 
 				}
+			System.out.println(); //added line break at the end of each row
 			}
 		}
 	}
