937819d1e39df33dc0255f59b46eeacf7a1f042f
loulai <louiselai88@gmail.com>
2014-11-06 13:54:08 -0500

    rough bubble sort implemented for part 1, courses. Stopping now and moving on to movies

--- ClassWork/src/Main.java
+++ ClassWork/src/Main.java
@@ -2,7 +2,17 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		String[] myS = new String[10];
-		System.out.println(myS[0]);
+		String string1 = "hello";
+		String string2 = "hot";
+		String string3 = "coffee";
+		String string4 = "hello";
+		
+		String s5 = "an";
+		String s6 = "apple";
+		String s7 = "boy";
+		
+		int answer = s7.compareToIgnoreCase(s6);
+		
+		System.out.println(answer);
 	}
 }
