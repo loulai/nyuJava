120c06540de3258ca7aa059cdeb6d0e1c27bb555
loulai <louiselai88@gmail.com>
2014-11-15 11:42:31 -0500

    overarching goal of project outlined with comments

---
+++ aaHomeworks/src/hw8matrix/Matrix.java
@@ -0,0 +1,33 @@
+package hw8matrix;
+
+public class Matrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public Matrix(){
+		
+	}
+
+}
+
+/** constructor with 2 params, row column
+ * - fill with random generated numbers (1-10)
+ * - 1-5 inclusive
+ * - if < 0, then default to one
+ * - if < 5, default to 5
+ * - 
+ * 
+ * - getNumRows, getNumColumns
+ * - all matrix methods
+ * - toString()
+ * - add(matrix) <so this is an instance method>
+ * - multiply(3) <another instance method>
+ */
+
+/** DRIVER
+ * 
+ * 
+ */
