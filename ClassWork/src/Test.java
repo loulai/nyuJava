30411672ecabda6377ca0871a3472a55cdab5316
loulai <louiselai88@gmail.com>
2014-12-02 21:17:41 -0500

    worked through problem of parallel processing and time

--- ClassWork/src/Test.java
+++ ClassWork/src/Test.java
@@ -2,17 +2,14 @@
 public class Test {
 
 	public static void main(String[] args) {
-		int val = 4;
-		int[] arr = {1,3,4,5,4};
-		for (int i =0; i < arr.length; i++){
-			if(arr[i] == val){
-				arr[i] = val;
-			}
+		long startTime = System.currentTimeMillis();
+		
+		for(int i = 0; i < 100; i++ ){
 			
-		for (int j = 0; j < arr.length; j++) {
-			System.out.print(arr[j]);
 		}
 		
-	}
+		long endTime = System.currentTimeMillis();
+		
+		System.out.println(startTime - endTime);
 	}
 }
--- ProcessingGame/src/ProcessingTest.java
+++ ProcessingGame/src/ProcessingTest.java
@@ -3,30 +3,32 @@ import processing.core.PApplet;
 public class ProcessingTest extends PApplet {	
 	
 	int timeCounter = 0;
-	Disk[] totalDisks = new Disk[8];
+	Disk[] totalDisks = new Disk[5];
 	int score = 0;
 	int counter = 0;
+	int printCounter = 0;
 	
 	public void setup() {
 		
 		size(800,600);
 		background(255);
 		
-		for(int i = 0; i < 8; i++){
+		for(int i = 0; i < 5; i++){
 			totalDisks[i] = new Disk(this);
 		}
 		
 	}
 	
+	long startTime = System.currentTimeMillis();
 	public void draw() {
-		timeCounter++;
-		if (timeCounter < 400) {
-			background(100,100,100);
+		if (timeCounter < 1000) {
+			
+			background(100,100,100);	
 			
 			for(int i = 0; i < totalDisks.length; i ++) {
 				Disk d = totalDisks[i];
+				
 				fill(100,100,100);
-				ellipse(d.getX(),d.getY(), d.getRX(),d.getRY());
 				d.move();
 				
 				textSize(20);
@@ -34,12 +36,40 @@ public class ProcessingTest extends PApplet {
 				textAlign(CENTER);
 				text(d.getValue(), d.getX(), d.getY());
 				
+				
+				if(timeSince(startTime) > 2000 && printCounter == 0 && startTime % 1000){
+					printCounter ++;
+					System.out.println("hello");
+				}
+				
+				
+//				boolean again = true;
+//	
+//				if(timeSince(startTime) % 1000 < 5) {
+//					if(again == true){
+//						again = false;
+//						System.out.println(timeSince(startTime));
+//					}
+//				}
+				
+			
+				
+//				if(timeCounter != 0 && timeCounter % 100 == 0){
+//					System.out.println("Removing Disk at " + timeCounter);
+//					Disk[] leftovers = new Disk[totalDisks.length - 1];
+//					
+//					for(int h = 0; h < totalDisks.length - 1; h++){
+//						leftovers[h] = totalDisks[h];
+//	
+//					}
+//					totalDisks = leftovers;
+//				}
+				
 				if(mousePressed){
 					if(mouseX >= (d.getX() - 30) && mouseX <= (d.getX() + 30) && mouseY >= (d.getY() - 30) && mouseY <= (d.getY() + 30)){
 						score = score + d.getValue();
-						d.setNewCoordinates();
-						Disk[] newDisks = new Disk[totalDisks.length - 1];
 						
+						Disk[] newDisks = new Disk[totalDisks.length - 1];
 						for (int k = 0; k < totalDisks.length; k++){
 							if (i != k){
 								newDisks[counter] = totalDisks[k];
@@ -51,7 +81,9 @@ public class ProcessingTest extends PApplet {
 					}
 				}
 				text(score, 70,70);
+				
 			}
+			timeCounter+=1;
 			
 		} else {
 			background(100,100,100);
@@ -59,5 +91,10 @@ public class ProcessingTest extends PApplet {
 			text(score, 400,300);
 		}
 		
+	}
+
+	public static int timeSince(long startTime){
+		long currentTime = System.currentTimeMillis();
+		return (int)(currentTime - startTime);
 	}
 }
