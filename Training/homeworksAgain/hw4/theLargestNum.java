6f72e1e5f4ae21882fed1ccfef023374f36f80bd
loulai <louiselai88@gmail.com>
2014-12-27 16:34:58 +0900

    DONE, works for finding largest and second largest

--- Training/homeworksAgain/hw4/theLargestNum.java
+++ Training/homeworksAgain/hw4/theLargestNum.java
@@ -9,13 +9,27 @@ public class theLargestNum {
 		
 		int n1 = input.nextInt();
 		int n2 = input.nextInt();
-		int largest = n1;
+		int largest = n2;
+		int secondLargest = n1;
 		
 		if(n2 == 0) System.out.println("too few integers, at least 2 numbers please");
+		if(n1 > n2) {
+			secondLargest = largest;
+			largest = n1;
+		}
 		
-		if(n2 > n1) largest  = n2;
+		while(n1 != 0){ //compare nextInt() with the last largest integer from last movement
+			n1 = input.nextInt();
+			if(n1 > largest) {
+				secondLargest = largest;
+				largest = n1;
+			}
+			if(n1 < largest && n1 > secondLargest) secondLargest = n1;
+		}
 		
-		System.out.println("=== LARGEST INTEGER: " + largest + " ===");
+		System.out.println("===     LARGEST INTEGER: " + largest + " ===");
+		System.out.println("=== 2ND LARGEST INTEGER: " + secondLargest + " ===");
+	
 		
 	}
 
