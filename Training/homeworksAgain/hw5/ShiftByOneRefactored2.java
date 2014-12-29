2320a81b4c3d09a6b6a4a88dc47f91b99bdde176
loulai <louiselai88@gmail.com>
2014-12-29 12:56:50 +0900

    created shiftbyx in record speed! All works, comments included

--- Training/homeworksAgain/hw5/ShiftByOneRefactored2.java
+++ Training/homeworksAgain/hw5/ShiftByOneRefactored2.java
@@ -8,7 +8,7 @@ public class ShiftByOneRefactored2 {
 		String s = input.nextLine();
 		char[] letters = s.toCharArray();
 		
-		char storer; //doesn't matter what this is
+		char storer; //initialize with nothing, because will fill storer within the for loop later on
 		char swapper = letters[letters.length-1]; //load swapper up with the last character of the string
 		
 		for(int i = 0; i < letters.length; i++){
@@ -16,7 +16,6 @@ public class ShiftByOneRefactored2 {
 			letters[i] = swapper;         // like firstly, pour CHAR into STORE (store it). Then pour whatever is in SWAPPER into the now 'empty' cup of CHAR. 
 			swapper = storer;             // Finally, load up SWAPPER by what was in STORE, in preparation for the next iteration.
 		}
-		
 		System.out.printf("Original : %s\n", s);
 		System.out.printf("Shifted  : %s", new String(letters));
 	}
