872f0fd7a216a6bd7e4aa1db6c138c847eff1499
loulai <louiselai88@gmail.com>
2014-12-31 01:11:20 +0900

    includes check to see if given array is sorted or not with two while loops. Output is nicer formatted.

--- Training/homeworksAgain/hw5/SortedMerge.java
+++ Training/homeworksAgain/hw5/SortedMerge.java
@@ -1,3 +1,6 @@
+/**
+ * takes two sorted arrays and merges them together. The end array is sorted.
+ */
 package hw5;
 
 import java.util.Scanner;
@@ -5,26 +8,49 @@ import java.util.Scanner;
 public class SortedMerge {
 	
 	public static void main(String[] args) {
-		//getting user input. First size of the array, then the content. Same process for the second array.
-		System.out.print("Size of first array: ");
-		Scanner input1 = new Scanner(System.in);
-		int size1 = input1.nextInt();
-		double[] list1 = new double[size1];
-		System.out.printf("Input %s numbers from smallest to largest, and end with enter:", size1);
-		Scanner input2 = new Scanner(System.in);
-		for(int i = 0; i < size1; i++){
-			list1[i] = input2.nextDouble();
+		
+		boolean again1 = true;
+		int size1 = 1; double[] list1 = new double[size1]; //must initialize outside while loop! It doesn't matter what the size is, it just needs to be initialized
+		while(again1){
+			//getting user input. First size of the array, then the content. Same process for the second array.
+			System.out.print("===#1\nSize of first array: ");
+			Scanner input1 = new Scanner(System.in);
+			size1 = input1.nextInt();
+			list1 = new double[size1];
+			System.out.printf("Input %s numbers from smallest to largest, and end with enter:", size1);
+			Scanner input2 = new Scanner(System.in);
+			for(int i = 0; i < size1; i++){
+				list1[i] = input2.nextDouble();
+			}
+			
+			if(isSorted(list1)){
+				again1 = false;
+			} else {
+				System.out.println(">>>>> Array is not sorted from smallest to largest; you shall not pass. Try again. <<<<<"); //again is still set to true
+			}
+			
 		}
 		
-		System.out.print("===\nSize of second array: ");
-		Scanner input3 = new Scanner(System.in);
-		int size2 = input3.nextInt();
-		double[] list2 = new double[size2];
-		System.out.printf("Input %s numbers from smallest to largest, and end with enter:", size2);
-		Scanner input4 = new Scanner(System.in);
-		for(int i = 0; i < size2; i++){
-			list2[i] = input4.nextDouble();
+		boolean again2 = true;
+		int size2 = 1; double[] list2 = new double[1]; 
+		while(again2){
+			System.out.print("===#2\nSize of second array: ");
+			Scanner input3 = new Scanner(System.in);
+			size2 = input3.nextInt();
+			list2 = new double[size2];
+			System.out.printf("Input %s numbers from smallest to largest, and end with enter:", size2);
+			Scanner input4 = new Scanner(System.in);
+			for(int i = 0; i < size2; i++){
+				list2[i] = input4.nextDouble();
+			}
+			
+			if(isSorted(list2)){
+				again2 = false;
+			} else{
+				System.out.println(">>>>> Not sorted. None for you Gretchen. Try again.  <<<<<");
+			}
 		}
+		
 		
 		double[] total = new double[size1 + size2]; //creating new array size of both entered arrays
 		
@@ -40,6 +66,7 @@ public class SortedMerge {
 		
 		for(int i = 0; i < total.length; i++){
 			
+			/*filling in final array with whichever value is smallest*/
 			if(c1 < size1 && c2 < size2){
 				
 				if(list1[c1] < list2[c2]){
@@ -63,12 +90,33 @@ public class SortedMerge {
 			}
 			
 		}
+		//nice output
+		System.out.printf("\n========   First Array (%d):    ===============\n", size1);
+		for(int i = 0; i < size1; i++){
+			System.out.printf("[%3.1f] ",list1[i]);
+		}	
 		
-		System.out.printf("======== New Array (%d): ===============\n", total.length);
+		System.out.printf("\n========  Second Array (%d):   ================\n", size2);
+		for(int i = 0; i < size2; i++){
+			System.out.printf("[%3.1f] ",list2[i]);
+		}	
+		System.out.printf("\n>>>========  New Array (%d): ===============<<<\n", total.length);
 		for(int i = 0; i < total.length; i++){
 			System.out.printf("[%3.1f] ",total[i]);
-		}
-		
+		}	
 	}
-	
+
+	/** checks to see is a given list of doubles is sorted from smallest to largest (ascending) order.
+	 * @param list double[] List that needs to be determined if sorted or not
+	 * @return      boolean  true if it is sorted, false otherwise
+	 */
+	private static boolean isSorted(double[] list){
+		boolean isSorted = true;
+		for(int i =  0; i < list.length - 1; i++){
+			if(list[i] > list[i + 1]){
+				isSorted = false;
+			}
+		}
+		return isSorted;
+	}
 }
