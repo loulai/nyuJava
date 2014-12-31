8872de7c8516cbfca2ebfa22fb35fc86418f0032
loulai <louiselai88@gmail.com>
2014-12-31 13:04:56 +0900

    kata complete ;)

--- Training/src/findSecondLargest/FindSecondLargestAgain.java
+++ Training/src/findSecondLargest/FindSecondLargestAgain.java
@@ -3,8 +3,24 @@ package findSecondLargest;
 public class FindSecondLargestAgain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] nums = {1,6,4,3};
+		System.out.println(findSecondLargest(nums));
+	}
+	
+	public static int findSecondLargest(int[] nums){
+		int largest = nums[0];
+		int secondLargest = nums[0];
+		
+		for(int i = 0; i < nums.length; i++){
+			if(nums[i] > largest){
+				secondLargest = largest;
+				largest = nums[i];
+			}
+			if(nums[i] > secondLargest && nums[i] < largest){
+				secondLargest = nums[i];
+			}
+		}
+		return secondLargest;
 	}
 
 }
