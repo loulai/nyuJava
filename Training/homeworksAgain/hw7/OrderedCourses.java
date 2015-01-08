c1647dbd738322797d3deaa186f4165d0df9f624
loulai <louiselai88@gmail.com>
2015-01-08 12:07:32 +0800

    course generaton done for first course, will do more now

--- Training/homeworksAgain/hw7/OrderedCourses.java
+++ Training/homeworksAgain/hw7/OrderedCourses.java
@@ -3,8 +3,19 @@ package hw7;
 public class OrderedCourses {
 	
 	public static void main(String[] args){
-		Course mine = new Course();
-		System.out.println(mine.getInstructor());
+		String[] courseNames = {"dragons"};
+		String[] courseInstructor = {"Targaryen"};
+		int[] courseCapacity = {3000};
+		int[] courseNumOfStudents =  {2900};
+		String[] courseCode = {"A101"};
+		
+		Course[] listOfCourses = new Course[10];
+		for(int i = 0; i < 1; i++){
+			listOfCourses[i] = new Course(courseNames[i], courseInstructor[i], courseCapacity[i], courseNumOfStudents[i],courseCode[i]);
+		}
+		
+		System.out.print(listOfCourses[0].getInstructor());
+		
 	}
 	
 
