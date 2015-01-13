ae8cbeab08d89d0f7a678046b60e6d680e08daad
loulai <louiselai88@gmail.com>
2015-01-13 00:30:48 -0800

    added comments

--- Training/src/linearSearch/LinearSearch.java
+++ Training/src/linearSearch/LinearSearch.java
@@ -8,12 +8,17 @@ public class LinearSearch {
 		String[] w1 = {"yo", "homeless", "man", "hello"};
 		String[] w2 = {"yo", "homeless", "man", "buhbye"};
 
-		System.out.println(LinearSearch.includes(w2, key));
+		System.out.println(LinearSearch.includes(key, w2));
 	}
 	
-	public static boolean includes(String[] arr, String key){
-		boolean contains = false;
-		for(int i = 0; i < arr.length; i++){
+	/** method searches an array to determine if a string is contained within the array. Returns true if found, else returns false.
+	 * @param key     the String in question; the String to be searched for
+	 * @param arr     the array to be searched through for the key
+	 * @return boolean true if string is included in array, false otherwise
+	 */
+	public static boolean includes(String key, String[] arr){
+		boolean contains = false; //false unless something is found that sets this varaiable to true
+		for(int i = 0; i < arr.length; i++){ //loop through length of the array
 			if(arr[i].equalsIgnoreCase(key)) contains = true;
 		}
 		return contains;
