21729d5fbbae5138bc0b21227267e6632d1a3057
loulai <louiselai88@gmail.com>
2015-02-22 21:17:20 -0500

    added all supporting classes

--- Calculator Assignment/src/calc/Calculator.java
+++ Calculator Assignment/src/calc/Calculator.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Calculator {
 	public static void main(String[] args){
 		System.out.println("input equation:");
-		Scanner s = new Scanner(System.in);
-		System.out.println(s.nextInt());
-		System.out.println(s.next());
+		Scanner t = new Scanner(System.in);
+		System.out.println(t.nextInt());
+		System.out.println(t.next());
 	}
 	
 }
