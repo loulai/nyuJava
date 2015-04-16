01258bfb8434714b1f9254858d8016bcf346955b
loulai <louiselai88@gmail.com>
2015-04-16 12:55:19 -0400

    added methods

---
+++ Huffman/src/HuffmanTree.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class HuffmanTree {
+	
+	HuffmanNode root;
+
+	public static void main(String[] args) throws IOException{
+//		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//		while(true){
+//			String line;
+//			if(line== null) break;
+//			System.out.println(line);
+//		}
+	
+	}
+	
+	
+
+}
